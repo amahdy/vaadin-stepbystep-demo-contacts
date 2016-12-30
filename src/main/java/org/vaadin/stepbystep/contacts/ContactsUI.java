75af6dcf2db98f2350c946e77416735198932800
AMahdy AbdElAziz <amahdy7@gmail.com>
2016-12-30 11:37:01 +0200

    Step 5 | 04:55 | Populate backend data into the grid

--- src/main/java/org/vaadin/stepbystep/contacts/ContactsUI.java
+++ src/main/java/org/vaadin/stepbystep/contacts/ContactsUI.java
@@ -1,19 +1,18 @@
 package org.vaadin.stepbystep.contacts;
 
-import javax.servlet.annotation.WebServlet;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.vaadin.stepbystep.person.backend.Person;
+import org.vaadin.stepbystep.person.backend.PersonService;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -28,6 +27,17 @@ public class ContactsUI extends UI {
 
 	HorizontalSplitPanel splitter = new HorizontalSplitPanel();
 	Grid grid = new Grid();
+	
+    @Inject
+    PersonService service;
+
+    @PostConstruct
+    void load() {
+        service.loadData();
+
+        BeanItemContainer<Person> container = new BeanItemContainer<>(Person.class, service.getEntries());
+        grid.setContainerDataSource(container);
+    }
 	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
