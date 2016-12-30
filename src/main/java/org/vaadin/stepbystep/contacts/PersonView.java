29e8bac0c32447a1caa5922cb701ea552557a6c5
AMahdy AbdElAziz <amahdy7@gmail.com>
2016-12-30 12:03:57 +0200

    Step 11 | 10:15 | Save changes to the local object in the binder

--- src/main/java/org/vaadin/stepbystep/contacts/PersonView.java
+++ src/main/java/org/vaadin/stepbystep/contacts/PersonView.java
@@ -3,6 +3,7 @@ package org.vaadin.stepbystep.contacts;
 import org.vaadin.stepbystep.person.backend.Person;
 
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 
 public class PersonView extends PersonDesign {
 
@@ -10,6 +11,14 @@ public class PersonView extends PersonDesign {
 	
 	public PersonView() {
 		binder.bindMemberFields(this);
+		
+		save.addClickListener(evt -> {
+			try {
+				binder.commit();
+			} catch (CommitException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 	
 	public void setPerson(Person selectedRow) {
