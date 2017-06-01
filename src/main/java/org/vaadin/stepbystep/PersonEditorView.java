d497b2680d9aef835a6c3c54d7dcae5769ff7d31
AMahdy AbdElAziz <amahdy7@gmail.com>
2017-06-01 20:28:17 +0300

    Step 9 | 09:00 | Define data binder for the form

--- src/main/java/org/vaadin/stepbystep/PersonEditorView.java
+++ src/main/java/org/vaadin/stepbystep/PersonEditorView.java
@@ -1,11 +1,20 @@
 package org.vaadin.stepbystep;
 
+import com.vaadin.data.Binder;
 import org.vaadin.stepbystep.person.backend.Person;
 
 /**
  * Created by amahdy on 6/1/17.
  */
 public class PersonEditorView extends PersonEditorDesign {
+
+    Binder<Person> binder = new Binder<>(Person.class);
+
+    public PersonEditorView() {
+        binder.bindInstanceFields(this);
+    }
+
     public void setPerson(Person value) {
+        binder.setBean(value);
     }
 }
